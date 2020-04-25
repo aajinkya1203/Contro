c6645eb1cb06e6407d178fc5ee49a83d4419459e
Aajinkya <aajinkya1203@gmail.com>
2020-04-26 01:59:28 +0530

    added some more

--- contro/src/App.js
+++ contro/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render(){
---
+++ contro/src/component/layout/IndexPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const IndexPage=()=> {
+    return (
+        <div className="container">
+            <div className="">
+                
+            </div>
+        </div>
+    )
+}
+
+export default IndexPage
