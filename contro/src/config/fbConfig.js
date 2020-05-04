c514da44a0d42b84bb794addd71a06abf2b0664f
Aajinkya Singh <43881544+aajinkya1203@users.noreply.github.com>
2020-05-04 15:06:51 +0530

    updated some files

--- contro/src/config/fbConfig.js
+++ contro/src/config/fbConfig.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyAUlk-S0khLCkRvmgEFBvhXJOJdZZplGMw",
-authDomain: "controrary.firebaseapp.com",
-databaseURL: "https://controrary.firebaseio.com",
-projectId: "controrary",
-storageBucket: "controrary.appspot.com",
-messagingSenderId: "518129156787",
-appId: "1:518129156787:web:6d3a025861ec34a324e6c6",
-measurementId: "G-E5BCBDPEDK"
+//add firebase config data
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
